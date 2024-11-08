f36c4f2a84bcbad832888c212874c074fb6aa87f
A-O-Emmanuel <emmanuel0058@gmail.com>
2024-11-08 07:44:00 +0100

    implemented user signin functionality

--- routes/signIn.js
+++ routes/signIn.js
@@ -1,15 +1,18 @@
+const {User} = require('../models/user');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const router = express.Router();
 
 
-router.post('/', (req, res) => {
-    const signInInfo = {
-        email: req.body.email,
-        password: req.body.password
-    }
-    
-    console.log(signInInfo)
-    res.send(signInInfo)
+router.post('/', async (req, res) => {
+    let user = await User.findOne({email: req.body.email});
+    if (!user) return res.status(400).send('Invalid email or password.');
+
+   const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if(!validPassword) return res.status(400).send('Invalid email or password.');
+
+    res.send(true);
+
 })
 
 module.exports = router;
