9bf8f5725accb0ccfec6b33d00588906f888997e
A-O-Emmanuel <emmanuel0058@gmail.com>
2024-11-13 05:33:37 +0100

    changed res.send to res.json in the in the sigin in route

--- routes/signIn.js
+++ routes/signIn.js
@@ -13,7 +13,7 @@ router.post('/', async (req, res) => {
     if(!validPassword) return res.status(400).send('Invalid email or password.');
 
     const token = jwt.sign({_id: user.id}, process.env.JWTPRIVATEKEY);
-    res.send(token);
+    res.json({token});
 
 })
 
