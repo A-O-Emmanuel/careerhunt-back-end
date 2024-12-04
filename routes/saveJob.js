3e33fcd14e10ea9ce802cd40fd2bf7d745ce4c99
A-O-Emmanuel <emmanuel0058@gmail.com>
2024-12-04 07:50:35 +0100

    corrected typographical errors in the ssavejob route

--- routes/saveJob.js
+++ routes/saveJob.js
@@ -9,9 +9,9 @@ const router = express.Router();
 router.post('/', auth,  async (req, res) => {
     try {const jobInfo = {
         jobTitle: req.body.jobTitle,
-        company: req.body.compay,
+        company: req.body.company,
         jobLocation: req.body.jobLocation,
-        salarMax: req.body.salaryMax,
+        salayMax: req.body.salaryMax,
         salaryMin: req.body.salaryMin,
         description: req.body.description,
         contract: req.body.contract,
