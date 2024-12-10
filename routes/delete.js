d60d32c562fec80898597229d0aca75f494f892f
A-O-Emmanuel <emmanuel0058@gmail.com>
2024-12-10 22:53:37 +0100

    added the delete functionality to the delete route, which deletes users from the database

--- routes/delete.js
+++ routes/delete.js
@@ -1,12 +1,12 @@
+const auth = require('../middleware/auth')
+const {User} = require('../models/user');
 const express = require('express');
 
 const router = express.Router();
 
-router.delete('/', (req, res) => {
-    const data = {
-        name: req.body.name
-    }
-    res.send(data)
+router.delete('/', auth, async (req, res) => {
+    const user = await User.deleteOne(req.user._id).select('-password')
+    console.log(user) 
 })
 
 module.exports = router;
