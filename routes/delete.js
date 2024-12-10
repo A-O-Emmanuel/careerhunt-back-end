a728b343fbea6574d88608f6e7046f97783fde1e
A-O-Emmanuel <emmanuel0058@gmail.com>
2024-12-11 00:21:45 +0100

    finished implementing the delete route to delete a user

--- routes/delete.js
+++ routes/delete.js
@@ -5,8 +5,9 @@ const express = require('express');
 const router = express.Router();
 
 router.delete('/', auth, async (req, res) => {
-    const user = await User.deleteOne(req.user._id).select('-password')
-    console.log(user) 
+    const id = req.user._id
+    const user = await User.deleteOne({_id: id})
+    res.json(`You've successfully deleted your account..`)
 })
 
 module.exports = router;
