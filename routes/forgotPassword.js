ffabe5da96a46c99794dd7f72cdd0d9c6343f5af
A-O-Emmanuel <emmanuel0058@gmail.com>
2024-12-24 20:31:51 +0100

    changed the http verb in the forgotPassword route to patch because only the password property of the user will be changed

--- routes/forgotPassword.js
+++ routes/forgotPassword.js
@@ -5,9 +5,19 @@ const express = require('express');
 const router = express.Router();
 
 
-router.put('/', async (req, res) => {
+router.patch('/', async (req, res) => {
     let user = await User.findOne({email: req.body.email});
     if (!user) return res.status(400).json({message:'Email is invalid.', id:'1'});
+
+    //check if passwords match
+    if (req.body.newPassword !== req.body.confirm) return res.status(422).json({message:'Passwords must match', id:'2'})
+
+    const salt = await bcrypt.genSalt(10);
+    const saltedPassword = await bcrypt.hash(req.body.newPassword, salt)
+    //select user password
+
+    //replace user password with new one.
+
 
    const validPassword = await bcrypt.compare(req.body.password, user.password)
     if(!validPassword) return res.status(422).json({message:'Password is Invalid', id: '2'});
