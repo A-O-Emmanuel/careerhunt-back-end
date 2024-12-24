ef670c474c72e8ba789ab1fa000b521c08f0814b
A-O-Emmanuel <emmanuel0058@gmail.com>
2024-12-24 07:53:03 +0100

    added the forgotPassword route

---
+++ routes/forgotPassword.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+const {User} = require('../models/user');
+const bcrypt = require('bcrypt');
+const express = require('express');
+const router = express.Router();
+
+
+router.put('/', async (req, res) => {
+    let user = await User.findOne({email: req.body.email});
+    if (!user) return res.status(400).json({message:'Email is invalid.', id:'1'});
+
+   const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if(!validPassword) return res.status(422).json({message:'Password is Invalid', id: '2'});
+
+    const token = jwt.sign({_id: user.id}, process.env.JWTPRIVATEKEY);
+    res.json({token});
+
+})
+
+module.exports = router;
