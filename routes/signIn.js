04227c75eed0901ba5fdedc9e18a5f60b5b14d8a
A-O-Emmanuel <emmanuel0058@gmail.com>
2024-12-24 02:36:52 +0100

    edited validation error message for the signin route

--- routes/signIn.js
+++ routes/signIn.js
@@ -7,10 +7,10 @@ const router = express.Router();
 
 router.post('/', async (req, res) => {
     let user = await User.findOne({email: req.body.email});
-    if (!user) return res.status(400).send('Invalid email or password.');
+    if (!user) return res.status(400).json({message:'Email is invalid.', id:'1'});
 
    const validPassword = await bcrypt.compare(req.body.password, user.password)
-    if(!validPassword) return res.status(400).send('Invalid email or password.');
+    if(!validPassword) return res.status(422).json({message:'Password is Invalid', id: '2'});
 
     const token = jwt.sign({_id: user.id}, process.env.JWTPRIVATEKEY);
     res.json({token});
